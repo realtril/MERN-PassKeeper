11193cf54b16910e8c6c03315a62c4e5d7093c77
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-10-07 22:21:36 +0300

    treating status 500 errors

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const { notFound } = require('./helpers/errorMiddleware');
+const { errorHandler } = require('./helpers/errorMiddleware');
+const colors = require('colors');
 
 dotenv.config();
 //connect to DB
 mongoose.connect(
   process.env.DB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true },
-  () => console.log('connected to DB!'),
+  () => console.log('connected to DB!'.cyan.bold.underline),
 );
 
 //Import Routes
@@ -22,6 +25,15 @@ app.use(express.json());
 app.use('/api/user', authRoute);
 app.use('/api/passwords', passwordsRoute);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is Up An Running on PORT ${process.env.PORT}!`),
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
 );
