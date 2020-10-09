620afdbd2d4a4e0293d5fc0c4fdf11426eb7816a
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-10-09 15:16:45 +0300

    adaptive markup for login and register is done for 320px,768px,1024px

--- frontend/src/Containers/Login/Login.js
+++ frontend/src/Containers/Login/Login.js
@@ -5,6 +5,7 @@ import { navigation } from '../../constants/navigation';
 import { ReactComponent as RegPic } from '../../icons/login.svg';
 import bgWave from '../../icons/wave.png';
 import { withStyles } from '@material-ui/core/styles';
+import 'normalize.css';
 import css from './Login.module.css';
 
 const CssTextField = withStyles(() => ({
@@ -27,52 +28,54 @@ const CssTextField = withStyles(() => ({
 const Login = () => {
   return (
     <>
-      <div className={css.login_wrapper}>
-        <div className={css.login_headers}>
-          <h2 className={css.login__main_title}>
-            WELCOME TO YOUR SIMPLER LIFE ONLINE
-          </h2>
-          <h5 className={css.login__title}>
-            Fill all your passwords, payments, and personal details across the
-            web, on any device.
-          </h5>
-        </div>
-      </div>
-      <img className={css.bg_wave} src={bgWave} alt="bg-wave" />
-      <div className={css.login__container}>
-        <div className={css.login__illustration}>
-          <RegPic className={css.login__illustration_pic} />
-        </div>
-        <form className={css.login__form}>
-          <h2 className={css.login__form_header}>Login</h2>
-          <div className={css.login__form_input_wrapper}>
-            <CssTextField
-              type="email"
-              label="E-mail"
-              name="email"
-              required={true}
-            />
-            <CssTextField
-              type="password"
-              label="Password"
-              name="password"
-              required={true}
-            />
+      <section className={css.login}>
+        <div className={css.login_wrapper}>
+          <div className={css.login_headers}>
+            <h2 className={css.login__main_title}>
+              WELCOME TO YOUR SIMPLER LIFE ONLINE
+            </h2>
+            <h5 className={css.login__title}>
+              Fill all your passwords, payments, and personal details across the
+              web, on any device.
+            </h5>
           </div>
-          <button type="submit" className={css.login__form_btn}>
-            login
-          </button>
-          <p className={css.login__form_have_account}>
-            No account yet?&#8192;
-            <NavLink
-              className={css.login__form_login_transfer}
-              to={navigation.registration}
-            >
-              Register
-            </NavLink>
-          </p>
-        </form>
-      </div>
+        </div>
+        <img className={css.bg_wave} src={bgWave} alt="bg-wave" />
+        <div className={css.login__container}>
+          <div className={css.login__illustration}>
+            <RegPic className={css.login__illustration_pic} />
+          </div>
+          <form className={css.login__form}>
+            <h2 className={css.login__form_header}>Login</h2>
+            <div className={css.login__form_input_wrapper}>
+              <CssTextField
+                type="email"
+                label="E-mail"
+                name="email"
+                required={true}
+              />
+              <CssTextField
+                type="password"
+                label="Password"
+                name="password"
+                required={true}
+              />
+            </div>
+            <button type="submit" className={css.login__form_btn}>
+              login
+            </button>
+            <p className={css.login__form_have_account}>
+              No account yet?&#8192;
+              <NavLink
+                className={css.login__form_login_transfer}
+                to={navigation.registration}
+              >
+                Register
+              </NavLink>
+            </p>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
--- frontend/src/Containers/Registration/Registration.js
+++ frontend/src/Containers/Registration/Registration.js
@@ -5,6 +5,7 @@ import { navigation } from '../../constants/navigation';
 import { ReactComponent as RegPic } from '../../icons/register.svg';
 import bgWave from '../../icons/wave.png';
 import { withStyles } from '@material-ui/core/styles';
+import 'normalize.css';
 import css from './Registration.module.css';
 
 const CssTextField = withStyles(() => ({
@@ -27,55 +28,57 @@ const CssTextField = withStyles(() => ({
 const Registration = () => {
   return (
     <>
-      <div className={css.register_wrapper}>
-        <div className={css.register_headers}>
-          <h2 className={css.register__main_title}>
-            Keep struggling with handling so many passwords in mind?
-          </h2>
-          <h5 className={css.register__title}>
-            Join to PassKeeper. It's free and easy to use.
-          </h5>
+      <section className={css.registration}>
+        <div className={css.register_wrapper}>
+          <div className={css.register_headers}>
+            <h2 className={css.register__main_title}>
+              Keep struggling with handling so many passwords in mind?
+            </h2>
+            <h5 className={css.register__title}>
+              Join to PassKeeper. It's free and easy to use.
+            </h5>
+          </div>
+          <img className={css.bg_wave} src={bgWave} alt="bg-wave" />
         </div>
-        <img className={css.bg_wave} src={bgWave} alt="bg-wave" />
-      </div>
-      <div className={css.register__container}>
-        <div className={css.register__illustration}>
-          <RegPic className={css.register__illustration_pic} />
+        <div className={css.register__container}>
+          <div className={css.register__illustration}>
+            <RegPic className={css.register__illustration_pic} />
+          </div>
+          <form className={css.register__form}>
+            <h2 className={css.register__form_header}>Registration</h2>
+            <CssTextField
+              type="text"
+              label="Username"
+              name="username"
+              required={true}
+            />
+            <CssTextField
+              type="email"
+              label="E-mail"
+              name="email"
+              required={true}
+            />
+            <CssTextField
+              type="password"
+              label="Password"
+              name="password"
+              required={true}
+            />
+            <button type="submit" className={css.register__form_btn}>
+              Register
+            </button>
+            <p className={css.register__form_have_account}>
+              Already a member?&#8192;
+              <NavLink
+                className={css.register__form_login_transfer}
+                to={navigation.login}
+              >
+                Login here
+              </NavLink>
+            </p>
+          </form>
         </div>
-        <form className={css.register__form}>
-          <h2 className={css.register__form_header}>Registration</h2>
-          <CssTextField
-            type="text"
-            label="Username"
-            name="username"
-            required={true}
-          />
-          <CssTextField
-            type="email"
-            label="E-mail"
-            name="email"
-            required={true}
-          />
-          <CssTextField
-            type="password"
-            label="Password"
-            name="password"
-            required={true}
-          />
-          <button type="submit" className={css.register__form_btn}>
-            Register
-          </button>
-          <p className={css.register__form_have_account}>
-            Already a member?&#8192;
-            <NavLink
-              className={css.register__form_login_transfer}
-              to={navigation.login}
-            >
-              Login here
-            </NavLink>
-          </p>
-        </form>
-      </div>
+      </section>
     </>
   );
 };
